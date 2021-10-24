0c97d481ae8f6d385b718ee3a743d0f3dc01eeb3
codywall <cody.wall@gmail.com>
2021-10-24 14:19:11 -0700

    Update block heading

--- src/components/BlockHeading.js
+++ src/components/BlockHeading.js
@@ -1,9 +1,19 @@
 import styled from "@emotion/styled";
 import React from "react";
-import Block from "../images/bbbLogo.png";
+import Block from "../images/bbbBlock.png";
 
 const HeadingWrapper = styled.div`
   width: 100%;
+  margin-left: 2.75rem;
+
+  img {
+    width: 3.5rem;
+  }
+
+  h3 {
+    margin-top: -1.2rem;
+    margin-left: 2.75rem;
+  }
 `;
 
 const BlockHeading = (props) => {
