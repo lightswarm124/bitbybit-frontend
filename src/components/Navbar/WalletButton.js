365e81d04468c84c2b9ec040f1a8796d79e79bf9
codywall <cody.wall@gmail.com>
2021-10-24 22:04:16 -0700

    Remove form functions from wallet button

--- src/components/Navbar/WalletButton.js
+++ src/components/Navbar/WalletButton.js
@@ -12,31 +12,6 @@ const AlertButton = styled(Button)`
 `;
 
 const WalletButton = () => {
-  const [simpleStorage, setSimpleStorage] = useState(undefined);
-  const [data, setData] = useState(undefined);
-
-  useEffect(() => {
-    const init = async () => {
-      const { simpleStorage } = await getBlockchain();
-      const data = await simpleStorage.readData();
-      setSimpleStorage(simpleStorage);
-      setData(data);
-    };
-    init();
-  }, []);
-
-  const updateData = async (e) => {
-    e.preventDefault();
-    const data = e.target.elements[0].value;
-    const tx = await simpleStorage.updateData(data);
-    await tx.wait();
-    const newData = await simpleStorage.readData();
-    setData(newData);
-  };
-
-  if (typeof simpleStorage === "undefined" || typeof data === "undefined") {
-    return "Loading...";
-  }
   const status = "disconnected";
   return (
     <>
