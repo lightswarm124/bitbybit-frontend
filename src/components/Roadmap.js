569a3a44fa368d8cacdc5bac8559c7e38fcc092a
codywall <cody.wall@gmail.com>
2021-10-26 15:53:22 -0700

    Roadmap styling

--- src/components/Roadmap.js
+++ src/components/Roadmap.js
@@ -5,15 +5,20 @@ import {
 import "react-vertical-timeline-component/style.min.css";
 import BlockHeading from "./BlockHeading";
 import ContentWrapper from "./ContentWrapper";
+import "../styles/roadmap.css";
+import { withTheme } from "@emotion/react";
+import Checkmark from "../images/checkmark.png";
 
 const Roadmap = () => {
+  const roadmapProgress = 1;
   return (
     <ContentWrapper>
       <BlockHeading title="Roadmap" />
-      <VerticalTimeline>
+      <VerticalTimeline lineColor="#FF2498">
         <VerticalTimelineElement
-          contentStyle={{ background: "rgb(33, 150, 243)", color: "black" }}
-          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          contentStyle={{ color: "black" }}
+          iconStyle={{ background: "#FF2498", color: "#fff" }}
+          icon={<img src={Checkmark} alt="" />}
         >
           <h3 className="vertical-timeline-element-title">Phase 1</h3>
           <ul>
@@ -22,14 +27,20 @@ const Roadmap = () => {
             <li>Smart Contract Creation</li>
           </ul>
         </VerticalTimelineElement>
-        <VerticalTimelineElement contentStyle={{ color: "black" }}>
+        <VerticalTimelineElement
+          iconStyle={{ background: "black" }}
+          contentStyle={{ color: "black" }}
+        >
           <h3 className="vertical-timeline-element-title">Phase 2</h3>
           <ul>
             <li>ICO Launch</li>
             <li>Marketing Push</li>
           </ul>
         </VerticalTimelineElement>
-        <VerticalTimelineElement contentStyle={{ color: "black" }}>
+        <VerticalTimelineElement
+          iconStyle={{ background: "black" }}
+          contentStyle={{ color: "black" }}
+        >
           <h3 className="vertical-timeline-element-title">Phase 3</h3>
           <ul>
             <li>Liquidity Pool Implementation</li>
@@ -37,14 +48,20 @@ const Roadmap = () => {
             <li>CoinMarketCap Listing</li>
           </ul>
         </VerticalTimelineElement>
-        <VerticalTimelineElement contentStyle={{ color: "black" }}>
+        <VerticalTimelineElement
+          iconStyle={{ background: "black" }}
+          contentStyle={{ color: "black" }}
+        >
           <h3 className="vertical-timeline-element-title">Phase 4</h3>
           <ul>
             <li>Smart Contract Audit</li>
             <li>Team Expansion</li>
           </ul>
         </VerticalTimelineElement>
-        <VerticalTimelineElement contentStyle={{ color: "black" }}>
+        <VerticalTimelineElement
+          iconStyle={{ background: "black" }}
+          contentStyle={{ color: "black" }}
+        >
           <h3 className="vertical-timeline-element-title">Phase 5</h3>
           <ul>
             <li>App Development</li>
