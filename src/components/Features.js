d408814741535c23c29a0aeb32d27e9d17945af5
codywall <cody.wall@gmail.com>
2021-10-27 21:34:57 -0700

    Add feature section

--- src/components/Features.js
+++ src/components/Features.js
@@ -2,19 +2,21 @@ import React from "react";
 import ContentWrapper from "./ContentWrapper";
 import BlockHeading from "./_ui/BlockHeading";
 import CharityIcon from "../images/icons/charity-icon.png";
-// import GlobeIcon from "../images/icons/globe-icon.png";
-// import PhoneIcon from "../images/icons/phone-icon.png";
+import GlobeIcon from "../images/icons/globe-icon.png";
+import PhoneIcon from "../images/icons/phone-icon.png";
 import styled from "@emotion/styled";
 
 const FeatureWrapper = styled.div`
   background-color: #220969;
   position: relative;
   z-index: 99999;
+  padding-bottom: 2rem;
 
   .feature {
     display: flex;
     flex-direction: row;
     margin-left: 3rem;
+    margin-bottom: 1rem;
 
     .icon-wrapper {
       background-color: #ff2498;
@@ -57,6 +59,27 @@ const Features = () => {
           <div className="text-wrapper">
             <h3>Passive Income and Donations</h3>
             <p>75% of liquidation goes to charity</p>
+          </div>
+        </div>
+        <div className="feature">
+          <div className="icon-wrapper">
+            <img src={GlobeIcon} alt="" />
+          </div>
+          <div className="text-wrapper">
+            <h3>Affect Global Change</h3>
+            <p>
+              The charity changes throughout the year in an effort to benefit
+              all
+            </p>
+          </div>
+        </div>
+        <div className="feature">
+          <div className="icon-wrapper">
+            <img src={PhoneIcon} alt="" />
+          </div>
+          <div className="text-wrapper">
+            <h3>Stay Updated</h3>
+            <p>Stay updated with regular social media updates</p>
           </div>
         </div>
       </ContentWrapper>
