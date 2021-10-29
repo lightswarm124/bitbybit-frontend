039644b02e0920d58fa1a03bd8849ac350fae6ff
codywall <cody.wall@gmail.com>
2021-10-29 10:51:02 -0700

    Use ajax for form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -32,14 +32,29 @@ const ContactSection = styled.div`
 `;
 
 const Contact = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    let myForm = document.getElementById("contactForm");
+    let formData = new FormData(myForm);
+    console.log(formData);
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams(formData).toString(),
+    })
+      .then(() => console.log("Form successfully submitted"))
+      .catch((error) => alert(error));
+  };
   return (
     <ContactSection>
       <ContentWrapper id="contact">
         <BlockHeading title="Contact Us" />
         <Form
+          id="contactForm"
           name="contact"
-          method="POST"
-          action="https://bitbybit.org/?success=true"
+          // method="POST"
+          onSubmit={handleSubmit}
+          // action="https://bitbybit.org/?success=true"
           data-netlify="true"
           data-netlify-honeypot="true"
           encType="application/x-www-form-urlencoded"
@@ -47,13 +62,12 @@ const Contact = () => {
           <input type="hidden" name="form-name" value="contact" />
           <Form.Group className="mb-3">
             <Form.Label>Name</Form.Label>
-            <Form.Control id="standard-basic" name="name" type="text" />
+            <Form.Control name="name" type="text" />
           </Form.Group>
           <br />
           <Form.Group className="mb-3">
             <Form.Label>Email address</Form.Label>
             <Form.Control
-              id="standard-basic"
               name="email"
               type="email"
               placeholder="name@example.com"
@@ -63,12 +77,7 @@ const Contact = () => {
           <br />
           <Form.Group className="mb-3" multiline="true">
             <Form.Label>Message</Form.Label>
-            <Form.Control
-              name="message"
-              as="textarea"
-              id="standard-multiline-static"
-              rows="3"
-            />
+            <Form.Control name="message" as="textarea" rows="3" />
           </Form.Group>
           <br />
           <div>
