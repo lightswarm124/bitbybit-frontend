96be6fc0b3d444e46c4b0fae3d3d46a89a2e3cf0
codywall <cody.wall@gmail.com>
2021-11-03 12:59:41 -0700

    Change buy button to button

--- src/App.js
+++ src/App.js
@@ -19,6 +19,31 @@ import Contact from "./components/Contact.js";
 import { Form } from "react-bootstrap";
 import "typeface-roboto";
 
+const BuyButton = styled.button`
+  box-sizing: border-box;
+  display: inline-block;
+  text-align: center;
+  padding: 0.65rem 1.25rem;
+  border-radius: 8px;
+  margin-right: 1.5rem;
+  color: white;
+  background-color: ${(props) =>
+    props.variant === "primary" ? "#480EF2" : "transparent"};
+  border: ${(props) =>
+    props.variant === "primary" ? "none" : "2px solid #480EF2"};
+  text-decoration: none;
+  &:hover:not(:disabled),
+  &:active:not(:disabled),
+  &:focus {
+    outline: 0;
+    cursor: pointer;
+  }
+  &:disabled {
+    opacity: 0.6;
+    filter: saturate(60%);
+  }
+`;
+
 const pageStyles = {
   margin: 0,
   maxWidth: "100%",
@@ -258,13 +283,13 @@ function App() {
                   {bbbAmount} BBB = {bnbAmount.toFixed(5)} BNB
                 </p>
               )}
-              <Button
+              <BuyButton
                 type="submit"
                 variant="primary"
                 style={{ marginRight: "1rem" }}
               >
                 Purchase
-              </Button>
+              </BuyButton>
               <Button onClick={closeModal} variant="secondary">
                 Cancel
               </Button>
